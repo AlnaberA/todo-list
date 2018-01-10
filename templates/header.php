728356b75c66bd5bf866ce2b823123d5aa201081
Alaa Al Naber <alnaberalaa@gmail.com>
2018-01-10 12:56:31 -0500

    Changed index, added header and css.

---
+++ templates/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<head>
+    <title>To-do</title>
+    <meta charset="UTF-8"><!--specifies character set website written with; Defined by Unicode standard-->
+    <meta name="description" content="todo-application">
+    <meta name="author" content="Alaa">
+   <link rel="stylesheet" href="css/style.css">
+   
+</head>
